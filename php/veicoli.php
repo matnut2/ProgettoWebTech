839ceb005a5933aa4ae8109a7d4bf6f257f35f18
matnut2 <62598618+matnut2@users.noreply.github.com>
2022-04-11 16:24:51 +0200

    update

--- php/veicoli.php
+++ php/veicoli.php
@@ -5,7 +5,7 @@
         <title>Veicoli - Auto Asta</title>
         <link rel="stylesheet" type="text/css" media="screen" href="../css/styleAlternative.css"/>
         <link rel="stylesheet" type="text/css" media="screen and (max-width:1200px), only screen and (max-width:1200px)"  href="../css/mobile.css"/>
-        <link rel="stylesheet" type="text/css" media="print" href="../css/printVeicoli.css"/>
+        <link rel="stylesheet" type="text/css" media="print" href="../css/print.css"/>
         <meta charset="UTF-8"/>
         <meta name="description" content="Homepage di Auto Asta"/>
         <meta name="keywords" content="auto, asta, homepage, principale, veicoli"/>
