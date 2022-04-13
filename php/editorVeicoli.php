0f584d7fe4f60ed55251f2cd4af38d1747d2af52
matnut2 <62598618+matnut2@users.noreply.github.com>
2022-04-13 11:39:14 +0200

    Upload Pagine PHP Modifica Veicolo

---
+++ php/editorVeicoli.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <link rel="icon" type="image/x-icon" href="../img/2061866.png"/>
+        <title>Modifica Veicolo - Auto Asta</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/styleAlternative.css"/>
+        <link rel="stylesheet" type="text/css" media="screen and (max-width:1200px), only screen and (max-width:1200px)"  href="../css/mobile.css"/>
+        <link rel="stylesheet" type="text/css" media="print" href="../css/print.css"/>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="Pagina di Modifica dei Veicoli di Auto Asta"/>
+        <meta name="keywords" content="auto, asta, edit, modifica, veicoli"/>
+        <meta name="author" content="Carlesso Niccolò, Pillon Matteo, Soldà Matteo, Veronese Andrea"/>       
+    </head>
+    <body>
+        <div class="globalDiv">     
+
+        <?php require_once ('header.php')?>
+        <div id='content'>
+        <?php require_once ('editor_page.php')?>
+        </div>
+
+        </div>   
+        <?php require_once ('../html/footer.html')?>
+        </div>
+    </body>
+</html>
