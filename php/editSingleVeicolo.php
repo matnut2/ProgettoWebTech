9e4a3e6a9074c94656ae934320cc6432126f6b11
Matteo Pillon <matteopillon98@gmail.com>
2022-08-28 20:33:49 +0200

    Fix modifica Veicolo

--- php/editSingleVeicolo.php
+++ php/editSingleVeicolo.php
@@ -1,7 +1,6 @@
 <?php 
     require_once ('session_Manager.php');
     require_once('page.php');
-    require_once('veicolo.php');
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
